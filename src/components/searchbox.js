a900736bd806a2fc99921965d1219de4864e90a8
Ubibimbap <72666983+Ubibimbap@users.noreply.github.com>
2020-11-12 19:31:13 +0100

    final version

---
+++ src/components/searchbox.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBox = ({ searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+        onChange={(event) => searchChange(event)}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
